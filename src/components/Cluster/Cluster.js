dab33ed167097341bfc7d255d201ba4dccf63458
Fernando <fernando.calvo.sanz@gmail.com>
2020-01-20 21:14:18 +0000

    Create Cluster component

---
+++ src/components/Cluster/Cluster.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Marker} from 'react-map-gl';
+
+function Cluster({
+  latitude,
+  longitude,
+  count,
+  total,
+  onClick,
+}) {
+  let padding = (count/total) * 50;
+  padding = padding > 20 ? 20 : padding;
+  return (
+    <Marker latitude={latitude} longitude={longitude}>
+      <div 
+        style={{
+          borderRadius: "50%",
+          border: "1px solid #FFF",
+          backgroundColor: "#007B99",
+          color: "#FFF",
+          fontSize: "1.2rem",
+          lineHeight: "25px",
+          textAlign: "center",
+          padding: `${padding}px`,
+          width: "25px",
+          height: "25px",
+        }}
+        onClick={onClick}>
+        {count}
+      </div>
+    </Marker>
+  );
+}
+
+export default Cluster;
