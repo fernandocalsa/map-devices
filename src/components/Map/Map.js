e1fbc5fd1fad2f75f14fe306a15b9001440d3685
Fernando <fernando.calvo.sanz@gmail.com>
2020-01-21 21:25:32 +0000

    Expose mapBounds and viewport from Map and accept props

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,21 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapGL, { NavigationControl } from 'react-map-gl';
 const mapbox_token = process.env.REACT_APP_MAPBOX
 
-function Map({children}) {
+function Map({children, onMove, latitude = 0, longitude = 0, zoom = 1}) {
+  useEffect(() => {
+    setViewport((viewport) => ({
+      ...viewport,
+      latitude,
+      longitude,
+      zoom,
+    }))
+  }, [latitude, longitude, zoom]);
+
   const [viewport, setViewport] = useState({
     width: "100%",
     height: "100vh",
-    latitude: 0,
-    longitude: 0,
-    zoom: 1
+    latitude,
+    longitude,
+    zoom,
   });
 
+  const onViewportChange = (viewport) => {
+    const mapBounds = mapRef?.getMap().getBounds()
+    if (onMove) {
+      onMove(
+        {
+          latitude: viewport.latitude,
+          longitude: viewport.longitude,
+          zoom: viewport.zoom,
+        },
+        mapBounds);
+    } else {
+      setViewport(viewport);
+    }
+  };
+
+  let mapRef;
   return (
     <ReactMapGL
+      ref={map => mapRef = map}
       mapboxApiAccessToken={mapbox_token}
+      onViewportChange={onViewportChange}
       {...viewport}
-      onViewportChange={setViewport}
     >
       {children}
       <div style={{position: 'absolute', right: 0}}>
